c70948b50c37c2573a9899b062c544c55633b66f
clock783 <aodunlade3@gmail.com>
2023-02-27 06:12:30 -0500

    Clean up comments

--- javascript.js
+++ javascript.js
@@ -2,19 +2,6 @@
 * The Odin Project
 * Creating a Rock Paper Scissors Game with Javascript
 * To be played from the browser console.
-
-* General Plan
-*   Write function to getComputerChoice. Return choice (not just log in console)
-*   Write function to getPlayerSelection. Input should be case insensitive and be either rock, paper or scissors.
-*   Write function playRound with inputs playerSelection & computerSelection. Return string "You win/You lose! X beats X"
-*   Write function called game(). Plays five rounds. keeps score and reports a winner at the end.
-*/
-
-/*
-* getComputerChoice()
-*   define array with rock, paper and scissors
-*   generate random number between 0 and 2.
-*   return item at random num position in array.
 */
 
 function getComputerChoice() {
@@ -23,15 +10,7 @@ function getComputerChoice() {
     return choices[randomNum];
 }
 
-/*
-* getPlayerSelection()
-*   prompt player to input choice
-*   make input all lower case. compare it with approved choices.
-*   if lowercase matches approved choices, accept input by logging 'you chose x'.
-*   otherwise, log not approved input. re-enter choice.
-*   keep doing this until player chooses correctly.
-*/
-
+//variable affects prompt message in getPlayerSelection()
 let isFirstPrompt = true;
 
 function getPlayerSelection() {
@@ -46,18 +25,12 @@ function getPlayerSelection() {
     
     if (!((input === 'rock') || (input === 'paper') || (input === 'scissors'))) {
         // if input is not accepted, recurse function with different prompt
-        // console.log('no match');
         return getPlayerSelection();
         // Need to change the message prompt if it is a retry
     } else {
-        // console.log('its a match!');
         return input;
     }
 }
-
-/*
-* playRound(playerSelection, computerSelection)
-*/
 
 function playRound(playerSelection, computerSelection){
     // Check tie scenario
